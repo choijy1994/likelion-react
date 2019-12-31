2a021091b7dcf0e4b4a8050bcdc8a5f2ee9703a3
Choijy <shvk54@gmail.com>
2019-12-31 15:05:58 +0900

    first commit

---
+++ djangobackend/myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\cjy\documents\final assignment\djangobackend\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
